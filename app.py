7f34fb41993543b03eea72195df3f9b8f158b58b
Sigala Hernandez <sigalahr11@gmail.com>
2017-07-25 16:43:36 -0700

    Added Features to Heroku

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:abc123@localhost:8889/blogz'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz:abc123@localhost:8889/blogz')
+
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'NJfpoqwenDFSljqnlkjqLK'
